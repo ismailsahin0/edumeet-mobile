34edc83227026c4a470511c2fc10202ab3d6884c
ismailsahin0 <51785702+ismailsahin0@users.noreply.github.com>
2022-02-21 02:42:46 +0300

    deleted for security

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,10 +1,1 @@
 
-export default firebaseConfig = {
-    apiKey: "AIzaSyDSgQa-U3rOgcxX4w__8LNl858YvGzQyQY",
-    authDomain: "edumeet-58d73.firebaseapp.com",
-    projectId: "edumeet-58d73",
-    storageBucket: "edumeet-58d73.appspot.com",
-    messagingSenderId: "43750306031",
-    appId: "1:43750306031:web:4d710b02247d3d87edeb1a",
-    measurementId: "G-XGCXGC36KY"
-};
